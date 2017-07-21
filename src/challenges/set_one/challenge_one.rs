8589bf44c4208b67c27794d13dc109b3f4bcb9dd
Eric S. Bullington <eric.s.bullington@gmail.com>
2017-07-21 19:11:18 -0400

    Simplify testing

--- src/challenges/set_one/challenge_one.rs
+++ src/challenges/set_one/challenge_one.rs
@@ -8,16 +8,11 @@ pub fn hex_to_base64(hex_string: &str) -> String {
     BASE64.encode(&decoded)
 }
 
-#[cfg(test)]
-mod tests {
 
-    use super::*;
-
-    #[test]
-    fn challenge_one() {
-        let hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-        let base_64_string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_owned();
-        assert_eq!(base_64_string, hex_to_base64(&hex_string));
-    }
-
+#[test]
+fn challenge_one() {
+    let hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let base_64_string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_owned();
+    assert_eq!(base_64_string, hex_to_base64(&hex_string));
 }
+
--- src/challenges/set_one/challenge_two.rs
+++ src/challenges/set_one/challenge_two.rs
@@ -11,17 +11,11 @@ pub fn fixed_xor(original_string: &str, xor_string: &str) -> String {
     HEXLOWER.encode(&xored)
 }
 
-#[cfg(test)]
-mod tests {
 
-    use super::*;
-
-    #[test]
-    fn challenge_two() {
-        let original_string = "1c0111001f010100061a024b53535009181c";
-        let xor_string = "686974207468652062756c6c277320657965";
-        let result_string = "746865206b696420646f6e277420706c6179";
-        assert_eq!(result_string, fixed_xor(&original_string, &xor_string));
-    }
-
+#[test]
+fn challenge_two() {
+    let original_string = "1c0111001f010100061a024b53535009181c";
+    let xor_string = "686974207468652062756c6c277320657965";
+    let result_string = "746865206b696420646f6e277420706c6179";
+    assert_eq!(result_string, fixed_xor(&original_string, &xor_string));
 }
