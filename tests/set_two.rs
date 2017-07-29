6c5b6645ab0a02ea9abc9be53af82c3bfe63d188
Eric S. Bullington <eric.s.bullington@gmail.com>
2017-07-29 13:51:06 +0000

    Make padding byte in challenge nine a parameter
    Instead of hardcoding a specific padding byte, convert function into one
    that accepts the padding byte as a parameter.

--- tests/set_two.rs
+++ tests/set_two.rs
@@ -5,6 +5,6 @@ use cryptopals::challenges::set_two::*;
 #[test]
 fn test_challenge_one() {
     let unpadded = b"YELLOW SUBMARINE";
-    let padded = challenge_nine::pad_bytes(unpadded, 20);
+    let padded = challenge_nine::pad_bytes(unpadded, 20, 0x04);
     assert_eq!("YELLOW SUBMARINE\x04\x04\x04\x04", String::from_utf8(padded).unwrap());
 }
